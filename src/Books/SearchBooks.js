3cb00a59b8275438fc698f1e3bec899654b5b236
nadavperetz <nadavperetz@gmail.com>
2017-09-15 14:10:37 -0300

    select book to shelf

--- src/Books/SearchBooks.js
+++ src/Books/SearchBooks.js
@@ -1,23 +1,54 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
-import PropTypes from 'prop-types'
+import * as BooksAPI from './BooksAPI'
+import Book from './Book'
+import sortBy from 'sort-by'
 
 class SearchBooks extends Component{
 
+    state = {
+        query: '',
+        books: []
+    };
+    updateQuery = (query) => {
+        this.setState({query : query.trim()});
+        BooksAPI.search(query, 10).then((books) =>
+            this.setState({books : books}))
+    };
+
+    changeShelf = (book, new_shelf) => {
+        BooksAPI.update(book, new_shelf).then((data) => {
+            let old_books = this.state.books;
+            const idx = (old_books.findIndex((old_book) => old_book.id = book.id) - 1);
+            book.shelf = new_shelf;
+            old_books.pop(idx);
+            this.setState({books: old_books}
+            )
+        })
+    };
 
     render() {
+        const {books, query} = this.state;
+        books.sort(sortBy('name'));
         return (
             <div className="search-books">
                 <div className="search-books-bar">
 
                     <Link to="/" className="close-search">Close</Link>
                     <div className="search-books-input-wrapper">
-                        <input type="text" placeholder="Search by title or author"/>
-
+                        <input type="text"
+                               placeholder="Search by title or author"
+                               value={query}
+                               onChange={(event) => this.updateQuery(event.target.value)}
+                        />
                     </div>
                 </div>
                 <div className="search-books-results">
-                    <ol className="books-grid"></ol>
+                    <ol className="books-grid">
+                        {books.map((book) =>
+                            <Book key={book.id} book={book} changeShelf={this.changeShelf}/>)
+                        }
+                    </ol>
                 </div>
             </div>
         )
