40e4a7c68539454016218892b277fc550144a8ca
nadavperetz <nadavperetz@gmail.com>
2017-10-17 12:31:11 -0200

    props to state in constructor

--- src/Books/Book.js
+++ src/Books/Book.js
@@ -4,64 +4,68 @@ import {Link} from 'react-router-dom'
 
 class Book extends Component {
 
-    state = {
-        shelf: this.props.book.shelf
+  constructor(props) {
+    super(props);
+    this.state = {
+      shelf: this.props.book.shelf
     };
+  }
 
-    static propTypes = {
-        book: PropTypes.object.isRequired,
-        bookSelected: PropTypes.func.isRequired,
-        changeShelf: PropTypes.func.isRequired,
-    };
+  static propTypes = {
+    book: PropTypes.object.isRequired,
+    bookSelected: PropTypes.func.isRequired,
+    changeShelf: PropTypes.func.isRequired,
+  };
 
-    changeSelectionShelf = (e) => {
-        if (e.target.value !== this.state.shelf) {
-            this.props.changeShelf(this.props.book, e.target.value)
-        }
-    };
-
-    bookSelected = (e) => {
-        const clicked = e.target.checked;
-        this.props.bookSelected(clicked, this.props.book);
-    };
-
-    render() {
-        const {book} = this.props;
-        const {shelf} = this.state;
-        // Found a book without smallThumbnail...
-        const smallThumbnail = (book.imageLinks !== undefined) ? book.imageLinks.smallThumbnail : "";
-        return (
-            <li>
-                <div className='book'>
-                    <div className="book-top">
-                        <Link to={"detail/" + book.id}>
-                            <div className='book-cover' style={{
-                                width: 128, height: 193,
-                                backgroundImage: `url("${smallThumbnail}")`
-                            }}>
-
-                            </div>
-                        </Link>
-                        <div className="book-shelf-changer">
-                            <select onChange={this.changeSelectionShelf} value={shelf}>
-                                <option value="none" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                            </select>
-                        </div>
-                    </div>
-
-                    <div className="book-title">
-                        <input type="checkbox" className="book-click-checkbox" id="check2"
-                               onChange={this.bookSelected}/>
-                        &nbsp;{book.title}
-                    </div>
-                    <div className="book-authors">{book.authors}</div>
-                </div>
-            </li>
-        )
+  changeSelectionShelf = (e) => {
+    if (e.target.value !== this.state.shelf) {
+      this.props.changeShelf(this.props.book, e.target.value)
     }
+  };
+
+  bookSelected = (e) => {
+    const clicked = e.target.checked;
+    this.props.bookSelected(clicked, this.props.book);
+  };
+
+  render() {
+    const {book} = this.props;
+    const {shelf} = this.state;
+    console.log(shelf);
+    // Found a book without smallThumbnail...
+    const smallThumbnail = (book.imageLinks !== undefined) ? book.imageLinks.smallThumbnail : "";
+    return (
+        <li>
+          <div className='book'>
+            <div className="book-top">
+              <Link to={"detail/" + book.id}>
+                <div className='book-cover' style={{
+                  width: 128, height: 193,
+                  backgroundImage: `url("${smallThumbnail}")`
+                }}>
+
+                </div>
+              </Link>
+              <div className="book-shelf-changer">
+                <select onChange={this.changeSelectionShelf} value={shelf}>
+                  <option value="none" disabled>Move to...</option>
+                  <option value="currentlyReading">Currently Reading</option>
+                  <option value="wantToRead">Want to Read</option>
+                  <option value="read">Read</option>
+                </select>
+              </div>
+            </div>
+
+            <div className="book-title">
+              <input type="checkbox" className="book-click-checkbox" id="check2"
+                     onChange={this.bookSelected}/>
+              &nbsp;{book.title}
+            </div>
+            <div className="book-authors">{book.authors}</div>
+          </div>
+        </li>
+    )
+  }
 }
 
 
