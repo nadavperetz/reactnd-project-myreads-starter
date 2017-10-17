807eb5698596d292b838582ac881e8b6a71a65ce
nadavperetz <nadavperetz@gmail.com>
2017-10-17 12:30:30 -0200

    identation

--- src/Books/BookShelf.js
+++ src/Books/BookShelf.js
@@ -8,104 +8,104 @@ import Spinner from '../Animation/Spinner'
 
 class BookShelf extends Component {
 
-    state = {
-        selectedBooks: [],
-    };
+  state = {
+    selectedBooks: [],
+  };
 
-    static propTypes = {
-        books: PropTypes.array.isRequired,
-        updateBooks: PropTypes.func.isRequired,
-        shelf: PropTypes.string.isRequired,
-        isAnyBookSelected: PropTypes.func.isRequired
-    };
+  static propTypes = {
+    books: PropTypes.array.isRequired,
+    updateBooks: PropTypes.func.isRequired,
+    shelf: PropTypes.string.isRequired,
+    isAnyBookSelected: PropTypes.func.isRequired
+  };
 
 
-    changeShelf = (book, new_shelf) => {
-        BooksAPI.update(book, new_shelf).then((data) => {
-            let old_books = this.props.books;
-            const idx = old_books.findIndex(old_book => old_book.id === book.id);
-            book.shelf = new_shelf;
-            old_books[idx] = book;
-            this.props.updateBooks(old_books);
-        })
-    };
+  changeShelf = (book, new_shelf) => {
+    BooksAPI.update(book, new_shelf).then((data) => {
+      let old_books = this.props.books;
+      const idx = old_books.findIndex(old_book => old_book.id === book.id);
+      book.shelf = new_shelf;
+      old_books[idx] = book;
+      this.props.updateBooks(old_books);
+    })
+  };
 
-    addSelectionToShelf = (e) => {
-        const newShelf = e.target.value;
-        for (let selectedBook of this.state.selectedBooks) {
-            this.changeShelf(selectedBook, newShelf)
-        }
-        this.setState({selectedBooks: []});
-        this.props.isAnyBookSelected(false);
+  addSelectionToShelf = (e) => {
+    const newShelf = e.target.value;
+    for (let selectedBook of this.state.selectedBooks) {
+      this.changeShelf(selectedBook, newShelf)
+    }
+    this.setState({selectedBooks: []});
+    this.props.isAnyBookSelected(false);
+
+  };
+
+  bookSelected = (checked, book) => {
+    let old_books = this.state.selectedBooks;
+    const idx = old_books.findIndex(old_book => old_book.id === book.id);
+    // Remove
+    if (!checked && idx >= 0) {
+      old_books.splice(idx, 1);
+    }
+    // Add
+    else if (checked && idx < 0) {
+      old_books.push(book);
 
     }
+    this.setState({selectedBooks: old_books});
+    if (this.state.selectedBooks.length > 0)
+      this.props.isAnyBookSelected(true);
+    else
+      this.props.isAnyBookSelected(false);
 
-    bookSelected = (checked, book) => {
-        let old_books = this.state.selectedBooks;
-        const idx = old_books.findIndex(old_book => old_book.id === book.id);
-        // Remove
-        if (!checked && idx >= 0) {
-            old_books.splice(idx, 1);
-        }
-        // Add
-        else if (checked && idx < 0) {
-            old_books.push(book);
+  };
 
-        }
-        this.setState({selectedBooks: old_books});
-        if (this.state.selectedBooks.length > 0)
-            this.props.isAnyBookSelected(true);
-        else
-            this.props.isAnyBookSelected(false);
-        
-    };
+  render() {
+    const {books, shelf} = this.props;
+    const {selectedBooks} = this.state;
+    let isSearch = false;
+    let showingBooks = books;
+    let anyBookSelected = selectedBooks.length > 0;
+    if (this.props.shelf === 'search')
+      isSearch = true;
+    else
+      showingBooks = books.filter((book) => book.shelf === shelf);
+    const classNameMaster = isSearch ? 'search-books-results' : 'bookshelf';
+    return (
 
-    render() {
-        const {books, shelf} = this.props;
-        const {selectedBooks} = this.state;
-        let isSearch = false;
-        let showingBooks = books;
-        let anyBookSelected = selectedBooks.length > 0;
-        if (this.props.shelf === 'search')
-            isSearch = true;
-        else
-           showingBooks = books.filter((book) => book.shelf === shelf);
-        const classNameMaster = isSearch ? 'search-books-results' : 'bookshelf';
-        return (
+        <div className={classNameMaster}>
+          <div className="bookshelf-books">
+            {(showingBooks.length > 0) ?
+                <ol className="books-grid">
+                  {showingBooks.map((book) =>
+                      <Book key={book.id}
+                            book={book}
+                            changeShelf={this.changeShelf}
+                            bookSelected={this.bookSelected}
+                      />)}
+                </ol>
+                :
+                !isSearch &&
+                <Spinner/>
 
-            <div className={classNameMaster}>
-                <div className="bookshelf-books">
-                    {(showingBooks.length > 0)?
-                        <ol className="books-grid">
-                            {showingBooks.map((book) =>
-                                <Book key={book.id}
-                                      book={book}
-                                      changeShelf={this.changeShelf}
-                                      bookSelected={this.bookSelected}
-                                />)}
-                        </ol>
-                        :
-                        !isSearch &&
-                        <Spinner/>
+            }
 
-                    }
+          </div>
+          {anyBookSelected &&
+          <div className="move-on-batch">
+            <select onChange={this.addSelectionToShelf} value='none'>
+              <option value="none" disabled></option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+            </select>
+          </div>
+          }
+        </div>
 
-                </div>
-                {anyBookSelected &&
-                    <div className="move-on-batch">
-                        <select onChange={this.addSelectionToShelf} value='none'>
-                            <option value="none" disabled> </option>
-                            <option value="currentlyReading">Currently Reading</option>
-                            <option value="wantToRead">Want to Read</option>
-                            <option value="read">Read</option>
-                        </select>
-                    </div>
-                }
-            </div>
-           
 
-        )
-    }
+    )
+  }
 }
 
 export default BookShelf
